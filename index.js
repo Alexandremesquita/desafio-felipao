5a714c0ac384342411792e9f00e74d3cd3f95a2c
Alexandre Mesquita <marryedoceria@gmail.com>
2024-07-23 09:18:22 -0300

    completo

---
+++ index.js
@@ -0,0 +1,52 @@
+let heroi = "Fenix";
+let experienciaXP = 1.000;
+
+while(experienciaXP >= 1.000){
+    experienciaXP =+ 1.000
+}
+
+switch (true){
+
+    case 1(experienciaXP == 1.000):
+    console.log("Ferro");
+    break;
+
+    case 2(experienciaXP > 1.000 && experienciaXP <= 2.000):
+    console.log("Bronze");
+    break;
+
+    case 3(experienciaXP > 2.000 && experienciaXP <= 5.000):
+    console.log("Prata");
+    break;
+
+    case 4(experienciaXP > 5.000 && experienciaXP <= 7.000):
+    console.log("Ouro");
+    break;
+
+    case 5(experienciaXP > 7.000 && experienciaXP <= 8.000):
+    console.log("Platina");
+    break;
+
+    case 6(experienciaXP > 8.000 && experienciaXP <= 9.000):
+    console.log("Ascendente");
+    break;
+
+    case 7(experienciaXP > 9.000 && experienciaXP <= 10.000):
+    console.log("Imortal");
+    break; 
+
+    case 8(experienciaXP == 10.001):
+    console.log("Radiante");
+    break;
+
+    
+}
+
+    console.log("O herói de nome" + heroi + "está no nível" + experienciaXP);
+
+
+
+
+
+
+
